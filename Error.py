7ac1f460b244b6d2a07ee210370819789a0b8422
skalermo <skalermo@gmail.com>
2020-04-08 19:10:28 +0200

    Update Error class

--- Error.py
+++ Error.py
@@ -2,17 +2,24 @@ import sys
 
 
 class ErrorCode:
+    TOKEN_BUILD_FAIL = 'Failed to build a token'
+    EXCEED_MAX_ID_SIZE = 'Exceeded max length of an identifier'
+
     UNEXPECTED_TOKEN = 'Unexpected token'
     ID_NOT_FOUND = 'Identifier not found'
     DUPLICATE_ID = 'Duplicate id found'
 
 
 class Error(Exception):
-    def __init__(self, error_code=None, token=None, message=None):
+    def __init__(self, message: str, error_code=None, token=None):
         # Hide exception traceback
         sys.tracebacklimit = 0
-
-        super().__init__(message)
+        m = error_code
+        if token is not None:
+            m += f" '{token}'"
+        else:
+            m += f' {message}'
+        super().__init__(m)
 
 
 class LexerError(Error):
