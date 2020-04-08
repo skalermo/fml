d12e3e1ea0149d4e1eaa12cee33a2f395be51483
skalermo <skalermo@gmail.com>
2020-04-08 23:21:52 +0200

    Test all tokens

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,31 +1,63 @@
 import unittest
 from Lexer import Lexer
-from Token import TokenType
+from Token import TokenType, Token, RESERVED_KEYWORDS
 from Source import FileSource
-from Error import LexerError
 
 
 class LexerTest(unittest.TestCase):
-    def test_easy_expressions(self):
-        file_source = FileSource('expressions/easy.txt')
+    def test_all_tokens(self):
+
+        expected_tokens = [
+            Token(t) for t in RESERVED_KEYWORDS.values()
+        ] + [
+            Token(TokenType.ID, 'a'),
+            Token(TokenType.ID, 'aaa'),
+            Token(TokenType.ID, 'a123'),
+            Token(TokenType.ID, 'a_'),
+            Token(TokenType.ID, 'a_123'),
+            Token(TokenType.ID, 'abc_def_123gh'),
+            Token(TokenType.SEMI),
+            Token(TokenType.COMMA),
+            Token(TokenType.COLON),
+            Token(TokenType.PLUS),
+            Token(TokenType.MINUS),
+            Token(TokenType.MUL),
+            Token(TokenType.FLOAT_DIV),
+            Token(TokenType.ASSIGN),
+            Token(TokenType.LPAREN),
+            Token(TokenType.RPAREN),
+            Token(TokenType.LBRACK),
+            Token(TokenType.RBRACK),
+            Token(TokenType.LCURB),
+            Token(TokenType.RCURB),
+            Token(TokenType.LESS),
+            Token(TokenType.GRE),
+            Token(TokenType.LEQ),
+            Token(TokenType.GEQ),
+            Token(TokenType.EQ),
+            Token(TokenType.NEQ),
+            Token(TokenType.POW),
+            Token(TokenType.SCALAR, 0),
+            Token(TokenType.SCALAR, 12),
+            Token(TokenType.SCALAR, 12.345),
+            Token(TokenType.SCALAR, 12.345),
+            Token(TokenType.SCALAR, float('12.345e6')),
+            Token(TokenType.SCALAR, float('12.345e-6')),
+            Token(TokenType.SCALAR, 0),
+            Token(TokenType.SCALAR, 0.01),
+            Token(TokenType.SCALAR, float('0.001e2')),
+            Token(TokenType.SCALAR, float('0.0001e-2'))
+        ]
+
+        file_source = FileSource('tokens/all_tokens.txt')
         lexer = Lexer(file_source)
-        while lexer.current_token.type != TokenType.ETX:
-            print(lexer.current_token)
+        for expected_token in expected_tokens:
+            token = lexer.current_token
+            self.assertEqual(expected_token.type, token.type)
+            self.assertEqual(expected_token.value, token.value)
             lexer.move_to_next_token()
 
-    # def test_moderate_expressions(self):
-    #     with open('expressions/moderate.txt') as file:
-    #         lexer = Lexer(file.read())
-    #
-    #     current_token = lexer.move_to_next_token()
-    #     print(current_token)
-    #     while current_token.type != TokenType.EOF:
-    #         print(current_token)
-    #         current_token = lexer.move_to_next_token()
-
-        # lexer = Lexer('3.')
-        # with self.assertRaises(LexerError) as context:
-        #     lexer.move_to_next_token()
+        self.assertEqual(Token(TokenType.ETX).type, lexer.current_token.type)
 
 
 if __name__ == '__main__':
