8c03d00957b17af77af59516a110737cc44c7e5d
skalermo <skalermo@gmail.com>
2020-04-10 21:22:53 +0200

    Custom exception handling

--- Error.py
+++ Error.py
@@ -1,6 +1,13 @@
 import sys
 
 
+# Custom exception handler
+def handler(exception_type, exception, traceback):
+    print(Error.message)
+
+sys.excepthook = handler
+
+
 class ErrorCode:
     TOKEN_BUILD_FAIL = 'Failed to build a token'
     EXCEED_MAX_ID_SIZE = 'Exceeded max length of an identifier'
@@ -12,14 +19,17 @@ class ErrorCode:
 
 class Error(Exception):
     def __init__(self, message: str, error_code=None, token=None):
-        # Hide exception traceback
+        # hide traceback of pycharm
         sys.tracebacklimit = 0
-        m = error_code
-        if token is not None:
-            m += f" '{token}'"
-        else:
-            m += f' {message}'
-        super().__init__(m)
+
+        self.error_code = error_code
+        self.token = token
+        self.message = message
+
+        Error.message = f'{self.__class__.__name__}: {error_code}. {message}'
+        # super().__init__(m)
+
+    message = ''
 
 
 class LexerError(Error):
