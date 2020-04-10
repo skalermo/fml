2271c990d62f5bf1078bbb3e98d216a7e40e85fe
skalermo <skalermo@gmail.com>
2020-04-10 21:21:20 +0200

    Complete lexer tests

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,13 +1,14 @@
 import unittest
 from Lexer import Lexer
 from Token import TokenType, Token, RESERVED_KEYWORDS
-from Source import FileSource
+from Source import FileSource, StringSource
+from Error import LexerError, ErrorCode
 
 
 class LexerTest(unittest.TestCase):
     def test_all_tokens(self):
 
-        expected_tokens = [
+        tokens = [
             Token(t) for t in RESERVED_KEYWORDS.values()
         ] + [
             Token(TokenType.ID, 'a'),
@@ -46,18 +47,79 @@ class LexerTest(unittest.TestCase):
             Token(TokenType.SCALAR, 0),
             Token(TokenType.SCALAR, 0.01),
             Token(TokenType.SCALAR, float('0.001e2')),
-            Token(TokenType.SCALAR, float('0.0001e-2'))
+            Token(TokenType.SCALAR, float('0.0001e-2')),
+            Token(TokenType.ETX)
         ]
 
         file_source = FileSource('tokens/all_tokens.txt')
         lexer = Lexer(file_source)
-        for expected_token in expected_tokens:
+        for expected_token in tokens:
             token = lexer.current_token
             self.assertEqual(expected_token.type, token.type)
             self.assertEqual(expected_token.value, token.value)
-            lexer.move_to_next_token()
+            lexer.build_next_token()
 
-        self.assertEqual(Token(TokenType.ETX).type, lexer.current_token.type)
+    def test_unknown_single_char_token(self):
+        string = '^'
+        string_source = StringSource(string)
+        with self.assertRaises(LexerError) as e:
+            Lexer(string_source)
+        self.assertNotEqual(ErrorCode.UNEXPECTED_TOKEN, e.exception.error_code)
+        self.assertEqual(ErrorCode.TOKEN_BUILD_FAIL, e.exception.error_code)
+
+    def test_etx_on_comment_line(self):
+        string = 'not_comment = 1; # a comment'
+        lexer = Lexer(StringSource(string))
+
+        tokens = [
+            Token(TokenType.ID, 'not_comment'),
+            Token(TokenType.ASSIGN),
+            Token(TokenType.SCALAR, 1),
+            Token(TokenType.SEMI),
+            Token(TokenType.ETX)
+        ]
+
+        for expected_token in tokens:
+            token = lexer.current_token
+            self.assertEqual(expected_token.type, token.type)
+            self.assertEqual(expected_token.value, token.value)
+            lexer.build_next_token()
+
+    def test_etx_in_unfinished_string(self):
+        string = '"some random string'
+        with self.assertRaises(LexerError):
+            lexer = Lexer(StringSource(string))
+            while lexer.current_token.type != TokenType.ETX:
+                lexer.build_next_token()
+
+    def test_too_long_id(self):
+        id = ''.join(['a']*128)
+
+        # should pass
+        try:
+            Lexer(StringSource(id))
+        except LexerError:
+            self.fail()
+
+        # should fail
+        id += 'a'
+        with self.assertRaises(LexerError) as e:
+            Lexer(StringSource(id))
+        self.assertEqual(ErrorCode.EXCEED_MAX_ID_SIZE, e.exception.error_code)
+
+    def test_scalar_starts_with_zeros(self):
+        string = '0'
+
+        # should pass
+        try:
+            Lexer(StringSource(string))
+        except LexerError:
+            self.fail()
+
+        string += '0'
+        # should fail
+        with self.assertRaises(LexerError):
+            Lexer(StringSource(string))
 
 
 if __name__ == '__main__':
