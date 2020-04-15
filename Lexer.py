6cef0ef49701418e85d27bb7accc401db92f7e1d
skalermo <skalermo@gmail.com>
2020-04-15 18:45:30 +0200

    Rearrange functions for better readability

--- Lexer.py
+++ Lexer.py
@@ -2,28 +2,50 @@ from Token import TokenType, Token, RESERVED_KEYWORDS
 from Error import LexerError, ErrorCode
 from Position import Position
 
-MAX_ID_LENGTH = 128
-
 
 class Lexer:
+    # Static variables
+    MAX_ID_LENGTH = 128
+    _building_methods = []
+    _similar_token_types = []
+
+    @classmethod
+    def set_static_vars(cls):
+        cls._building_methods.extend([
+            cls.try_to_build_scalar,
+            cls.try_to_build_id,
+            cls.try_to_build_string,
+            cls.try_to_build_similar_tokens,
+            cls.try_to_build_neq,  # not equal (!=) token
+            cls.try_to_build_single_char_token
+        ])
+        cls._similar_token_types.extend([
+            (TokenType.MUL, TokenType.POW),
+            (TokenType.LESS, TokenType.LEQ),
+            (TokenType.GRE, TokenType.GEQ),
+            (TokenType.ASSIGN, TokenType.EQ)
+        ])
+
     def __init__(self, source):
         self.current_token = None
         self.source = source
+        self.set_static_vars()
         self.build_next_token()
 
-    def error(self, error_code=None):
-        s = "'{lexeme}' line: {position.line} column: {position.column}".format(
-            lexeme=self.source.current_char,
-            position=Position(self.source)
-        )
-        raise LexerError(error_code=error_code, message=s)
+    def build_next_token(self):
+        while self.skip_comment() or self.skip_whitespace():
+            pass
 
-    def skip_whitespace(self):
-        skipped = 0
-        while self.source.current_char.isspace():
-            self.source.move_to_next_char()
-            skipped += 1
-        return skipped
+        position = Position(self.source)
+
+        for try_to_build_token in Lexer._building_methods:
+            if token := try_to_build_token(self):
+                token.position = position
+                self.current_token = token
+                return
+
+        # If no success in building a known token
+        self.error(error_code=ErrorCode.TOKEN_BUILD_FAIL)
 
     def skip_comment(self):
         if self.source.current_char == '#':
@@ -34,6 +56,13 @@ class Lexer:
             self.source.move_to_next_char()
             return True
         return False
+
+    def skip_whitespace(self):
+        skipped = 0
+        while self.source.current_char.isspace():
+            self.source.move_to_next_char()
+            skipped += 1
+        return skipped
 
     def try_to_build_scalar(self):
         if not self.source.current_char.isdigit():
@@ -96,13 +125,13 @@ class Lexer:
         length_of_result = 1
         self.source.move_to_next_char()
 
-        while length_of_result <= MAX_ID_LENGTH and \
+        while length_of_result <= Lexer.MAX_ID_LENGTH and \
                 (self.source.current_char.isalnum() or self.source.current_char == '_'):
             collected_chars.append(self.source.current_char)
             self.source.move_to_next_char()
             length_of_result += 1
 
-        if length_of_result > MAX_ID_LENGTH:
+        if length_of_result > Lexer.MAX_ID_LENGTH:
             self.error(error_code=ErrorCode.EXCEED_MAX_ID_SIZE)
 
         # convert to string
@@ -151,14 +180,6 @@ class Lexer:
             value=result,
         )
 
-    # Static variable
-    _similar_token_types = [
-        (TokenType.MUL, TokenType.POW),
-        (TokenType.LESS, TokenType.LEQ),
-        (TokenType.GRE, TokenType.GEQ),
-        (TokenType.ASSIGN, TokenType.EQ)
-    ]
-
     def try_to_build_similar_tokens(self):
         for type_1char, type_2chars in Lexer._similar_token_types:
             if token := self._generic_builder(type_1char, type_2chars):
@@ -200,29 +221,9 @@ class Lexer:
             self.source.move_to_next_char()
             return Token(type=token_type)
 
-    # Static variable
-    _building_methods = [
-        try_to_build_scalar,
-        try_to_build_id,
-        try_to_build_string,
-        try_to_build_similar_tokens,
-        try_to_build_neq,   # not equal (!=) token
-        try_to_build_single_char_token
-    ]
-
-    def build_next_token(self):
-        while self.skip_comment() or self.skip_whitespace():
-            pass
-
-        position = Position(self.source)
-
-        for try_to_build_token in Lexer._building_methods:
-            if token := try_to_build_token(self):
-                token.position = position
-                self.current_token = token
-                return
-
-        # If no success in building a known token
-        self.error(error_code=ErrorCode.TOKEN_BUILD_FAIL)
-
-
+    def error(self, error_code=None):
+        s = "'{lexeme}' line: {position.line} column: {position.column}".format(
+            lexeme=self.source.current_char,
+            position=Position(self.source)
+        )
+        raise LexerError(error_code=error_code, message=s)
