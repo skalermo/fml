e1305fd205fde75fa3e54763d2bb6660c76684f6
skalermo <skalermo@gmail.com>
2020-04-16 17:36:09 +0200

    Change parser:  - write parse_program()  - rearrange functions and vars a bit

--- Parser.py
+++ Parser.py
@@ -6,47 +6,66 @@ from grammatical_objects.Statement import CompoundStatement
 
 
 class Parser:
+    # Static variables
+    _parse_methods = []
+
+    @classmethod
+    def set_static_vars(cls):
+        cls._parse_methods.extend([
+            cls.try_to_parse_expression,
+            cls.try_to_parse_do_while_loop,
+            cls.try_to_parse_while_loop,
+            cls.try_to_parse_for_loop,
+            cls.try_to_parse_if_statement,
+            cls.try_to_parse_compound_statement
+        ])
+
     def __init__(self, lexer):
         self.lexer = lexer
-        program = self.parse_program()
-
-    def error(self, error_code=None, token=None):
-        raise ParserError(error_code=error_code, message='')
+        self.program = self.parse_program()
 
     def parse_program(self):
-        program = Program()
-        while self.lexer.current_token.type != TokenType.ETX:
-            if definition := self.try_to_parse_fun_definition():
-                program.add_fun_def(definition)
-            if statement := self.parse_statement():
-                program.add_statement(statement)
+        fun_defs = {}
+        statements = []
 
-        return program
+        while (definition := self.try_to_parse_fun_definition()) or\
+                (statement := self.parse_statement()):
+            if definition is not None:
+                fun_defs[definition.id] = definition
+            elif statement is not None:
+                statements.append(statement)
+
+        return Program(fun_defs, statements)
 
     def try_to_parse_fun_definition(self):
         if self.lexer.current_token.type != TokenType.FUN:
             return None
         self.lexer.build_next_token()
 
-        fun_def = FunctionDefinition()
-
         if self.lexer.current_token.type != TokenType.ID:
             self.error(error_code=ErrorCode.UNEXPECTED_TOKEN)
 
-        fun_def.id = self.lexer.current_token
+        fun_id = self.lexer.current_token.value
         self.lexer.build_next_token()
 
         if self.lexer.current_token.type != TokenType.LPAREN:
             self.error(error_code=ErrorCode.UNEXPECTED_TOKEN)
 
         self.lexer.build_next_token()
-        fun_def.add_parameters(self.parse_fun_parameters())
+        parameter_list = self.try_to_parse_fun_parameters()
 
-        fun_def.body = self.parse_statement()
-        if fun_def.body is None:
-            self.error(error_code=ErrorCode.UNEXPECTED_TOKEN)
+        statement = self.parse_statement()
+        # if fun_def.body is None:
+        #     self.error(error_code=ErrorCode.UNEXPECTED_TOKEN)
 
-        return fun_def
+        return FunctionDefinition(fun_id, parameter_list, statement)
+
+    def parse_statement(self):
+        for try_to_parse_statement in Parser._parse_methods:
+            if statement := try_to_parse_statement(self):
+                return statement
+
+        return None
 
     def try_to_parse_expression(self):
         pass
@@ -79,24 +98,7 @@ class Parser:
         self.lexer.build_next_token()
         return CompoundStatement(statement_list)
 
-    # static variable
-    _parse_methods = [
-        try_to_parse_expression,
-        try_to_parse_do_while_loop,
-        try_to_parse_while_loop,
-        try_to_parse_for_loop,
-        try_to_parse_if_statement,
-        try_to_parse_compound_statement
-    ]
-
-    def parse_statement(self):
-        for try_to_parse_statement in Parser._parse_methods:
-            if statement := try_to_parse_statement(self):
-                return statement
-
-        return None
-
-    def parse_fun_parameters(self):
+    def try_to_parse_fun_parameters(self):
         if self.lexer.current_token.type == TokenType.RPAREN:
             return []
 
@@ -122,19 +124,5 @@ class Parser:
 
         return parameter_list
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def error(self, error_code=None, token=None):
+        raise ParserError(error_code=error_code, message='')
