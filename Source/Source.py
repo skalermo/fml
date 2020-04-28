55903fdc5f4b5907a6f9fc3d176b01bd1c009b95
skalermo <skalermo@gmail.com>
2020-04-28 19:46:48 +0200

    Add functions: - update_context_start - get_source_type

--- Source/Source.py
+++ Source/Source.py
@@ -8,7 +8,7 @@ class Source:
 
         self.line = 1
         self.column = 1
-        self._pos = 1
+        self._current_pos = 1
 
         # start of new fun definition or statement
         self._last_context_start_pos = 1
@@ -21,17 +21,20 @@ class Source:
             return
 
         self.column += 1
-        self._pos += 1
+        self._current_pos += 1
 
         if self.current_char == '\n':
             self.line += 1
-            self.column = 0
+            self.column = 1
+
+    def update_context_start(self):
+        self._last_context_start_pos = self._current_pos
 
     def get_last_context(self):
         pass
 
-    def update_context_start(self):
-        self._last_context_start_pos = self._pos
+    def get_source_type(self):
+        pass
 
 
 class FileSource(Source):
@@ -44,9 +47,12 @@ class FileSource(Source):
         self._file.close()
 
     def get_last_context(self):
-        offset = self._pos - self._last_context_start_pos
+        offset = self._current_pos - self._last_context_start_pos
         self._file.seek(self._last_context_start_pos, 0)
         return self._file.read(offset)
+
+    def get_source_type(self):
+        return self._file.name
 
     @staticmethod
     def _read_in_chars(file_object, chunk_size=1024):
@@ -65,10 +71,13 @@ class FileSource(Source):
 
 class StringSource(Source):
     def __init__(self, string):
-        self.string = string
+        self._string = string
         text_generator = (char for char in string)
         super().__init__(text_generator)
 
     def get_last_context(self):
-        offset = self._pos-self._last_context_start_pos
-        return self.string[-offset:]
+        offset = self._current_pos - self._last_context_start_pos
+        return self._string[-offset:]
+
+    def get_source_type(self):
+        return "<string>"
