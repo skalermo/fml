7660a64ab75412f6998abcf3aeff24c95f8c68b5
skalermo <skalermo@gmail.com>
2020-04-29 21:54:43 +0200

    Changes: - Error objects now have expected token type (useful when debugging) - Add new error codes

--- Error.py
+++ Error.py
@@ -15,16 +15,22 @@ class ErrorCode:
 
     UNEXPECTED_TOKEN = 'Unexpected token'
     EXPECTED_STATEMENT = 'Expected statement'
+    EXPECTED_ID = 'Expected identifier'
     EXPECTED_SEMI = 'Expected semicolon'
+    EXPECTED_ITERABLE = 'Expected iterable'
+    EXPECTED_MTRX_ROW = 'Expected matrix row'
+    EXPECTED_MTRX_ITEM = 'Expected matrix item'
+
     MTRX_ROW_LEN_MISMATCH = 'Matrix should have rows of the same length'
-    RVAL_FAIL = 'Expected rvalue, got nothing'
+    RVAL_FAIL = 'Expected rvalue'
+    EMPTY_COND = 'Condition is empty'
 
     ID_NOT_FOUND = 'Identifier not found'
     DUPLICATE_ID = 'Duplicate id found'
 
 
 class Error(Exception):
-    def __init__(self, error_code=None, position=None, context='', source_type='', token=''):
+    def __init__(self, error_code=None, position=None, context='', source_type='', expected_token_type=None):
         # hide traceback of pycharm
         sys.tracebacklimit = 0
 
@@ -33,12 +39,15 @@ class Error(Exception):
         #   2. context
         #   3. pointer
         #   4. Error type, error message
-
+        expected = ''
+        if error_code == ErrorCode.UNEXPECTED_TOKEN and expected_token_type is not None:
+            expected = f', expected {expected_token_type}'
         error_source = f'File "{source_type}", line {position.line}, column {position.column}\n'
         pointer_line = ''.join([' '*(len(context)-1), '^\n'])
         context += '\n'
-        message = f'{type(self).__name__}: {error_code}\n'
+        message = f'{type(self).__name__}: {error_code}{expected}\n'
 
+        self.expected_token_type = expected_token_type
         self.error_code = error_code
         self.message = ''.join([error_source, context, pointer_line, message])
         super().__init__(self.message)
