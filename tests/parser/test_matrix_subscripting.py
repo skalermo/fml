273f39feb929df17d7ba83c1daad334e02f2ee2a
skalermo <skalermo@gmail.com>
2020-04-29 23:05:54 +0200

    Add tests for matrix subscripting

---
+++ tests/parser/test_matrix_subscripting.py
@@ -0,0 +1,36 @@
+import unittest
+from Lexer.Lexer import TokenType
+from Error import ErrorCode
+from tests.parser.utils import should_fail
+
+
+class TestMatrixSubscriptingFails(unittest.TestCase):
+    def test_no_semi(self):
+        s = 'm[a]'
+        should_fail(self, s, ErrorCode.UNEXPECTED_TOKEN, TokenType.SEMI)
+
+    def test_multiple_indices(self):
+        s = 'm[a, b, c]'
+        # expected two indices, got 3
+        should_fail(self, s, ErrorCode.UNEXPECTED_TOKEN, TokenType.RBRACK)
+
+    def test_empty_index(self):
+        s = 'm[];'
+        should_fail(self, s, ErrorCode.EXPECTED_EXPRESSION)
+
+    def test_colon_index(self):
+        s = 'm[:,];'
+        should_fail(self, s, ErrorCode.EXPECTED_EXPRESSION)
+
+        s = 'm[::];'
+        should_fail(self, s, ErrorCode.UNEXPECTED_TOKEN, TokenType.RBRACK)
+
+        s = 'm[:,a+:];'
+        should_fail(self, s, ErrorCode.RVAL_FAIL)
+
+        s = 'm[:,:+a];'
+        should_fail(self, s, ErrorCode.UNEXPECTED_TOKEN, TokenType.RBRACK)
+
+
+if __name__ == '__main__':
+    unittest.main()
