51ec42c03e4e888d4634dfb5b5c9cdda7d6473f6
skalermo <skalermo@gmail.com>
2020-05-02 10:04:58 +0200

    Make launcher functional

--- fmli.py
+++ fmli.py
@@ -1,19 +1,21 @@
-from Lexer.Lexer import Lexer
-from Source.Source import StringSource
+from argparse import ArgumentParser
+from Source.Source import FileSource
+from Interpreter.AstDumper import dumpAST
+
+
+def parseArguments() -> vars:
+    parser = ArgumentParser(prog='fmli.py', description='Fantastic Matrix Language Interpreter')
+    parser.add_argument('filename', type=str, metavar='filename', help='File to interpret')
+    parser.add_argument('-d', '--dump', action='store_true', default=False, help='Dump AST of provided source')
+    return vars(parser.parse_args())
 
 
 def main():
-    # args = parseArguments()
-    # filename = args['filename']
-    # file_source = FileSource(filename)
-    # lexer = Lexer(file_source)
-    # token = lexer.build_next_token()
-    # while token:
-    #     print(token)
-    #     token = lexer.build_next_token()
-    string = '...'
-    source = StringSource(string)
-    Lexer(source)
+    args = parseArguments()
+    print(args)
+    if args['dump']:
+        source = FileSource(args['filename'])
+        dumpAST(source)
 
 
 if __name__ == '__main__':
