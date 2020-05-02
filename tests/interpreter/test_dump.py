6b9026cdda85fcacc5674f0b53512dd12461d71d
skalermo <skalermo@gmail.com>
2020-05-02 10:07:38 +0200

    Add dump tests

--- tests/interpreter/test_dump.py
+++ tests/interpreter/test_dump.py
@@ -7,11 +7,39 @@ from Interpreter.AstDumper import AstDumper
 
 class TestAstDumper(unittest.TestCase):
     def test_something(self):
-        string_source = '{' \
-                        'a(1+3);' \
-                        'b(2+4);' \
-                        'c(5+6);' \
-                        '}'
+        string_source = '   fun partition(arr, low, high)' \
+                        '   {' \
+                        '       i = (low -1);' \
+                        '       pivot = arr[high];' \
+                        '       for (j in range)' \
+                        '           if (arr[j] < pivot)' \
+                        '           {' \
+                        '               i = i+1;' \
+                        '               temp = arr[i];' \
+                        '               arr[i] = arr[j];' \
+                        '               arr[j] = temp;' \
+                        '           }' \
+                        '       temp = arr[i+1];' \
+                        '       arr[i+1] = arr[high];' \
+                        '       arr[high] = temp;' \
+                        '       ret (i+1);' \
+                        '   }' \
+                        '   fun quickSort(arr, low, high)' \
+                        '       if (low < high)' \
+                        '       {' \
+                        '           pi = partition(arr, low, high);' \
+                        '           quickSort(arr, low, pi-1);' \
+                        '           quickSort(arr, pi+1, high);' \
+                        '       }'
+
+        s = 'x[2];\n' \
+            'a[1, 2];\n' \
+            'b[a];\n' \
+            'c[:];\n' \
+            'd[:, 1];\n' \
+            'e[1, :];\n' \
+            'f[:, :];'
+
         parser = Parser(Lexer(StringSource(string_source)))
         program = parser.parse_program()
         ast_dumper = AstDumper()
