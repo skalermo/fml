b81c8c1aa4e588b8de632925770231b97b4058cc
skalermo <skalermo@gmail.com>
2020-05-14 22:32:56 +0200

    Change file name and add function should_fail

--- tests/interpreter/interpret.py
+++
@@ -1,8 +0,0 @@
-from Interpreter.Interpreter import Interpreter
-from Parser.Parser import Parser
-from Source.Source import StringSource
-
-
-def interpret(string_source):
-    parser = Parser(StringSource(string_source))
-    return Interpreter().interpret(parser.parse_program())
