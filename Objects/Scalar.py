f7e1d2ca356d581a4480373181bcf330adb9d93a
skalermo <skalermo@gmail.com>
2020-05-14 12:54:35 +0200

    Add replace repr with str method, use repr to get python object

--- Objects/Scalar.py
+++ Objects/Scalar.py
@@ -6,12 +6,15 @@ class Scalar(AST):
         self.value = value
 
     def __repr__(self):
-        return f'{self.__class__.__name__}: {self.value}'
+        return self.value
 
     def __bool__(self):
         if self.value == 0:
             return False
         return True
+
+    def __str__(self):
+        return f'{self.__class__.__name__}: {self.value}'
 
     def __eq__(self, other):
         return self.value == other.value
--- Objects/String.py
+++ Objects/String.py
@@ -5,5 +5,8 @@ class String(AST):
     def __init__(self, string):
         self.value = string
 
-    def __repr__(self):
+    def __str__(self):
         return f'{self.__class__.__name__}'
+
+    def __repr__(self):
+        return self.value
