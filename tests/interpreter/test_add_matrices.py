ddd98adf0213ff58b556f724c5554af6e20fac4e
skalermo <skalermo@gmail.com>
2020-05-14 13:13:39 +0200

    Update tests

--- tests/interpreter/test_add_matrices.py
+++ tests/interpreter/test_add_matrices.py
@@ -9,8 +9,8 @@ class TestAddMatrices(unittest.TestCase):
             'ret a + b;'
         m = interpret(s)
         self.assertEqual((1, 2), m.shape)
-        self.assertEqual(3.0, m[0][0].value)
-        self.assertEqual(6.0, m[0][1].value)
+        m = m.to_py()
+        self.assertEqual([[3, 6]], m)
 
     def test_add_nested_matrices(self):
         s = 'a = [1, [1, 2], 2];' \
@@ -18,11 +18,17 @@ class TestAddMatrices(unittest.TestCase):
             'ret a + b;'
         m = interpret(s)
         self.assertEqual((1, 3), m.shape)
-        self.assertEqual(11.0, m[0][0].value)
-        self.assertEqual(5.0, m[0][2].value)
-        nested_matrix = m[0][1]
-        self.assertEqual(2.0, nested_matrix[0][0].value)
-        self.assertEqual(4.0, nested_matrix[0][1].value)
+        m = m.to_py()
+        self.assertEqual([[11, [[2, 4]], 5]], m)
+
+    def test_multirow_addition(self):
+        s = 'a = [1; 2; 3; 4];' \
+            'b = [4; 3; 2; 1];' \
+            'ret a + b;'
+        m = interpret(s)
+        self.assertEqual((4, 1), m.shape)
+        m = m.to_py()
+        self.assertEqual([[5], [5], [5], [5]], m)
 
 
 if __name__ == '__main__':
